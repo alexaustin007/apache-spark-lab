24f2372e968f4f60d48d83d631d5d8c0c08fb412
alexaustin007 <austinchettiar007@gmail.com>
2025-11-24 11:43:26 -0500

    some filters and aggregations

--- sparksesion_start.py
+++ sparksesion_start.py
@@ -5,6 +5,15 @@ spark = SparkSession.builder.appName("sparkMastery").getOrCreate()
 
 df = spark.read.csv('routes_enriched.csv', header=True, inferSchema=True)
 
-print(df.show(5))
+# print(df.show(5))
+
+# df.printSchema()
+# print(df.count())
+
+
+distance_above_5000 = df.filter(df.distance_km > 5000).groupBy("airline_name").count().orderBy("count", ascending=False).limit(10)
+distance_above_5000.show()
+
 spark.stop()
+
 
