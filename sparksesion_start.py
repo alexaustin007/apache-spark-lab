63a957da98762ac1187cab8dfda7774c44f6408b
alexaustin007 <austinchettiar007@gmail.com>
2025-11-24 17:59:32 -0500

    contains like

--- sparksesion_start.py
+++ sparksesion_start.py
@@ -8,23 +8,27 @@ df = spark.read.csv('routes_enriched.csv', header=True, inferSchema=True)
 
 
 
-# print(df.show(5))
+df.show(5)
 
-# df.printSchema()
-# print(df.count())
-
-
-# distance_above_5000 = df.filter(df.distance_km > 5000).groupBy("airline_name").count().orderBy("count", ascending=False).limit(10)
-# distance_above_5000.show()
+df.printSchema()
+print(df.count())
 
 
-# distance_above_8000 = df.filter((df.distance_km > 8000) & (df.stops == 0) & (df.origin_country == 'India')).groupBy("airline_name").count().orderBy("count", ascending=False).limit(10)
-# distance_above_8000.show()
+distance_above_5000 = df.filter(df.distance_km > 5000).groupBy("airline_name").count().orderBy("count", ascending=False).limit(10)
+distance_above_5000.show()
+
+
+distance_above_8000 = df.filter((df.distance_km > 8000) & (df.stops == 0) & (df.origin_country == 'India')).groupBy("airline_name").count().orderBy("count", ascending=False).limit(10)
+distance_above_8000.show()
 
 multiple_agg = df.groupBy("airline_name").agg(F.count('*').alias("total_flights"),
                                               F.avg("distance_km").alias("avg_distance"),
                                               F.max("seats").alias("max_seats")).orderBy("total_flights", ascending=False).limit(10)
 multiple_agg.show()
+
+contains_new = df.filter(df.origin_city.like("%New%")).groupBy("origin_city").count().orderBy("count", ascending=False)
+
+contains_new.show()
 
 spark.stop()
 
