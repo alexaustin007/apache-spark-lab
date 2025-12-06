e6a9148fec8a02b62e20988b4d815a13dcea393e
alexaustin007 <austinchettiar007@gmail.com>
2025-12-06 12:19:13 +0530

    checking job UI for how shuffle works for groupBy, sum and count

--- spark_shuffles_joins.py
+++ spark_shuffles_joins.py
@@ -11,7 +11,12 @@ orders_df = spark.read.csv('orders.csv', header = True, inferSchema = True)
 spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "-1")
 
 joined_df = customer_df.join(orders_df, on='customer_id', how = "inner")
-joined_df.show()
+# joined_df.show()
+
+total_amount_spend_customer = joined_df.groupBy("customer_id", "name").agg(F.sum("amount").alias("total_amount_spend_by_customer"),
+                                                                           F.count("order_id").alias("order_count")).orderBy(F.desc("total_amount_spend_by_customer"))
+
+total_amount_spend_customer.show()
 
 print("Spark UI is running at: http://localhost:4040")
 print("Press Ctrl+C to stop...")
