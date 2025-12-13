a53877846e54bed3cf26e625e88c57c315c7f2be
alexaustin007 <austinchettiar007@gmail.com>
2025-12-13 23:52:23 +0530

    understanding working of coalesce efficiency writing pyspark jobs

--- spark_partitioning.py
+++ spark_partitioning.py
@@ -6,11 +6,16 @@ df = spark.read.csv("routes_enriched.csv", inferSchema = True, header = True)
 
 # df.show(5)
 # print(df.count())
-print(df.rdd.getNumPartitions())
-print(f"Total rows: {df.count()}")
+# print(df.rdd.getNumPartitions())
+# print(f"Total rows: {df.count()}")
 repartitioned_df = df.repartition(20)
-print(f"Partitions after repartition(20): {repartitioned_df.rdd.getNumPartitions()}")
+# print(f"Partitions after repartition(20): {repartitioned_df.rdd.getNumPartitions()}")
 
   # Trigger action and check UI
-repartitioned_df.count()
+# repartitioned_df.count()
+
+coalesced_df = repartitioned_df.coalesce(5)
+print(coalesced_df.rdd.getNumPartitions())
+coalesced_df.count()
+
 input("Press Enter to stop...")
